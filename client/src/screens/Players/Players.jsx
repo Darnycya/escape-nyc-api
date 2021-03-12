eedc4b657ce9f573f5443737c8aedb168e0dd217
Darnycya <Darnycya@gmail.com>
2021-03-12 14:38:39 -0500

    created react app

---
+++ client/src/screens/Players/Players.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react'
+import './Products.css'
+
+import Product from '../../components/Product/Product'
+import Search from '../../components/Search/Search'
+import { AZ, ZA, lowestFirst, highestFirst } from "../../utils/sort"
+import Sort from '../../components/Sort/Sort'
+import Layout from '../../components/shared/Layout/Layout'
+import { getPlayers } from '../../services/players'
+
+const Players = (props) => {
+  const [allPlayers, setAllPlayers] = useState([])
+  const [queriedPlayers, setQueriedPlayers] = useState([])
+  const [sortType, setSortType] = useState([])
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      const players = await getPlayers()
+      setAllPlayers(players)
+      setQueriedPlayers(players)
+    }
+    fetchPlayers()
+  }, [])
+
+  const handleSort = type => {
+    setSortType(type)
+    switch (type) {
+      case "name-ascending":
+        setQueriedPlayers(AZ(queriedPlayers))
+        break
+      case "name-descending":
+        setQueriedPlayers(ZA(queriedPlayers))
+        break
+      case "price-ascending":
+        setQueriedPlayers(lowestFirst(queriedPlayers))
+        break
+      case "price-descending":
+        setQueriedPlayers(highestFirst(queriedPlayers))
+        break
+      default:
+        break
+    }
+  }
+
+  const handleSearch = event => {
+    const newQueriedPlayers = allPlayers.filter(player => player.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    setQueriedPlayers(newQueriedPlayers, () => handleSort(sortType))
+  }
+
+  const handleSubmit = event => event.preventDefault()
+
+  const playersJSX = queriedPlayers.map((player, index) =>
+    <Player _id={player._id} name={player.name} image={player.image} position={player.position} jerseyNumber={player.jerseyNumber} key={index} />
+  )
+
+  return (
+    <>
+      <Search onSubmit={handleSubmit} onChange={handleSearch} />
+      <Sort onSubmit={handleSubmit} onChange={handleSort} />
+      <div className="players">
+        {playersJSX}
+      </div>
+    </>
+  )
+}
+
+
+export default Products
