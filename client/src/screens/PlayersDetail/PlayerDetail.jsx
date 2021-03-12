e22e71e384e359a3535857dfa9f77625dc9befc0
Darnycya <Darnycya@gmail.com>
2021-03-12 15:22:01 -0500

    created PlayerDetail

---
+++ client/src/screens/PlayersDetail/PlayerDetail.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import './PlayerDetail.css'
+import { getPlayer, deletePlayer } from '../../services/players'
+import { useParams, Link } from 'react-router-dom'
+
+const PlayerDetail = (props) => {
+
+    const [player, setPlayer] = useState(null)
+    const [isLoaded, setLoaded] = useState(false)
+    const { id } = useParams()
+
+    useEffect(() => {
+        const fetchPlayer = async () => {
+            const player = await getPlayer(id)
+            setPlayer(player)
+            setLoaded(true)
+        }
+        fetchplayer()
+    }, [id])
+
+    if (!isLoaded) {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <Layout>
+            <div className="player-detail">
+                <img className="player-detail-image" src={player.image} alt={player.name} />
+                <div className="detail">
+                    <div className="name">{player.name}</div>
+                    <div className="position">{player.position}</div>
+                    <div className="jerseyNumber">{player.jerseyNumber}</div>
+                    <div className="button-container">
+                        <button className="edit-button"><Link className="edit-link" to={`/players/${player._id}/edit`}>Edit</Link></button>
+                        <button className="delete-button" onClick={() => deleteplayer(player._id)}>Delete</button>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default PlayerDetail
