348d67873157c92eb8dad5560bdd82de8b4d09f3
Darnycya <Darnycya@gmail.com>
2021-03-13 21:01:44 -0500

    playerDetail css

--- client/src/screens/PlayerDetail/PlayerDetail.jsx
+++ client/src/screens/PlayerDetail/PlayerDetail.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { getPlayer, deletePlayer } from '../../services/players'
 import { useParams, Link } from 'react-router-dom'
 import Logo from '../../components/Logo/Logo'
+import './PlayerDetail.css';
 
 const PlayerDetail = (props) => {
 
@@ -28,9 +29,7 @@ const PlayerDetail = (props) => {
             <div className="player-detail">
                 <img className="player-detail-image" src={player.image} alt={player.name} />
                 <div className="detail">
-                    <div className="name">{player.name}</div>
-                    <div className="position">{player.position}</div>
-                    <div className="jerseyNumber">{player.jerseyNumber}</div>
+                    
                     <div className="button-container">
                         <button className="edit-button"><Link className="edit-link" to={`/players/${player._id}/edit`}>Edit</Link></button>
                         <button className="delete-button" onClick={() => deletePlayer(player._id)}>Delete</button>
