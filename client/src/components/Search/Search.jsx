1e136255314e6580943f28f26b48e671c49b0f38
Darnycya <Darnycya@gmail.com>
2021-03-13 19:28:10 -0500

    Search Component created

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,17 +1,20 @@
-  
-import React from 'react';
-import './Product.css';
-import { Link } from 'react-router-dom'
+import React from 'react'
+import './Search.css'
 
-const Product = (props) => {
+const Search = (props) => {
     return (
-        <>
-            <Link className="product" to={`/products/${props._id}`}>
-                <img className="product-image" src={props.imgURL} alt={props.name} />
-                <div className="product-name">{props.name}</div>
-                <div className="price">{`$${props.price}`}</div>
-            </Link>
-        </>
+        <form className="search-form" onSubmit={(e) => props.onSubmit(e)}>
+            <input
+                className="search-input"
+                value={props.value}
+                onChange={(e) => props.onChange(e)}
+                name="Search"
+                placeholder="Search"
+                type="text"
+                autoFocus
+            />
+        </form>
     )
 }
-export default Product
+
+export default Search
