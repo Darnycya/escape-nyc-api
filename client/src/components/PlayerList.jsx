281ff048617ab70776f55730cb621fa1619fb03a
Darnycya <Darnycya@gmail.com>
2021-03-13 19:23:05 -0500

    Player Component

---
+++ client/src/components/PlayerList.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './Player.css';
+import { Link } from 'react-router-dom'
+
+const Player = (props) => {
+    return (
+        <>
+            <Link className="player" to={`/players/${props._id}`}>
+                <img className="player-image" src={props.image} alt={props.name} />
+            </Link>
+        </>
+    )
+}
+export default Player
