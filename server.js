1bac2c07814301315f68050b5b341efb4241ef7a
Darnycya <Darnycya@gmail.com>
2021-08-25 13:47:26 -0400

    created api

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
-const PORT = process.env.PORT || 3000
-const db = require('./db/connection')
-const Player = require('./models/player')
-
-
 const cors = require('cors')
-
+const PORT = process.env.PORT || 8080
 
 const app = express()
 
@@ -15,77 +11,81 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(logger('dev'))
 
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
 })
 
-
-
-app.get('/', (req, res) => res.send("This is root!"))
-
-
-app.get('/players', async (req, res) => {
-    try {
-        const players = await Player.find()
-        res.json(players)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: error.message })
+const trails =
+  [
+    {
+      "id": "1",
+      "name": "Anthony's Nose",
+      "trailUrl": "https://hikethehudsonvalley.com/wp-content/uploads/2015/05/Spring2014_RWAN_955.jpg",
+      "difficulty": "Medium",
+      "distanceFromNyc": 40,
+      "climbingTime": 2.5,
+      "rating": 4
+    },
+    {
+      "id": "2",
+      "name": "Breakneck Ridge",
+      "trailUrl": "https://www.nynjtc.org/sites/default/files/styles/hike_park_destination_page/public/hotw/BreakneckRidgePanorama_EduardoGil2014.jpg?itok=KOE4oHkO",
+      "difficulty": "Hard",
+      "distanceFromNyc": 50,
+      "climbingTime": 4,
+      "rating": 5
+    },
+    {
+      "id": "3",
+      "name": "Sam's Point",
+      "trailUrl": "https://scenichudson.org/wp-content/uploads/2019/10/OA_Sams-Point01_Diana-Richards-1400x1050.jpg",
+      "difficulty": "Medium",
+      "distanceFromNyc": 79,
+      "climbingTime": 3,
+      "rating": 5
+    },
+    {
+      "id": "4",
+      "name": "Bash Bish Falls",
+      "trailUrl": "https://www.scenichudson.org/wp-content/uploads/2019/10/OA_Bash-Bish_Diana-Richards.jpg",
+      "difficulty": "Easy",
+      "distanceFromNyc": 106,
+      "climbingTime": 1,
+      "rating": 4
     }
+  ]
+
+app.get('/', (req, res) => {
+  res.send("This is root!");
+});
+
+app.get('/trails', async (req, res) => {
+  res.json(trails)
 })
 
-
-app.get('/players/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const player = await Player.findById(id)
-        res.json(player)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: error.message })
-    }
+app.get('/trails/:id', async (req, res) => {
+  const id = req.params.id
+  const trail = trails.filter(trail => trail.id === id)[0]
+  res.json(trail)
 })
 
-
-app.post('/players', async (req, res) => {
-    try {
-        const player = new Player(req.body)
-        await player.save()
-        res.status(201).json(player)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: error.message })
-    }
+app.post('/trails', (req, res) => {
+  const trail = req.body
+  trails.push(trail)
+  res.json(trails)
 })
 
-
-
-app.put('/players/:id', async (req, res) => {
-    const { id } = req.params
-    await Player.findByIdAndUpdate(id, req.body, { new: true }, (error, player) => {
-        if (error) {
-            return res.status(500).json({ error: error.message })
-        }
-        if (!player) {
-            return res.status(404).json(player)
-        }
-        res.status(200).json(player)
-    })
+app.put('/trails/:id', async (req, res) => {
+  const id = req.params.id
+  const trailIndex = trails.findIndex(trail => trail._id === id)
+  const trail = { ...trails[trailIndex], ...req.body }
+  trails.splice(trailIndex, 1, trail)
+  res.json(trail)
 })
 
-
-app.delete('/players/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const deleted = await Player.findByIdAndDelete(id)
-        if (deleted) {
-            return res.status(200).send("Player deleted")
-        }
-        throw new Error("Player not deleted!")
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
+app.delete('/trails/:id', async (req, res) => {
+  const id = req.params.id
+  const trailIndex = trails.findIndex(trail => trail._id === id)
+  trails.splice(trailIndex, 1)
+  res.json(trails)
 })
